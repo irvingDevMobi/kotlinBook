77d9fce7aae9be7d868faa50cbed0a80fe1b17fe
Irving Lop <irving.lopez@globant.com>
2018-02-02 12:20:07 -0600

    Chapter 11
    Adding operators to our ListForecast model
    
    Signed by: Irving Lop <irvingloop@gmail.com>

--- app/src/main/java/lopez/irving/kotlinbook/domain/model/DomainClasses.kt
+++ app/src/main/java/lopez/irving/kotlinbook/domain/model/DomainClasses.kt
@@ -5,6 +5,11 @@ package lopez.irving.kotlinbook.domain.model
  * @author irving.lopez
  * @since 31/01/2018.
  */
-data class ForecastList(val ciy: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val ciy: String, val country: String, private val dailyForecast: List<Forecast>) {
+    val size: Int
+        get() = dailyForecast.size
+
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+}
 
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
--- app/src/main/java/lopez/irving/kotlinbook/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/lopez/irving/kotlinbook/ui/adapters/ForecastListAdapter.kt
@@ -18,13 +18,13 @@ class ForecastListAdapter(private val weekForecast: ForecastList) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        with(weekForecast.dailyForecast[position]) {
+        with(weekForecast[position]) {
             holder.textView.text = "$date - $description - $high/$low"
         }
     }
 
     override fun getItemCount(): Int {
-        return weekForecast.dailyForecast.size
+        return weekForecast.size
     }
 
     class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
