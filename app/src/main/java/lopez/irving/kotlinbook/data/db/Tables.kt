7df6722bf00ef5fb3e544cc65fb0b424875492a3
Irving Lop <irving.lopez@globant.com>
2018-03-12 16:53:10 -0600

    Chapter 17
    Using SqLite helper class provided by Anko SQL extension
    
    Signed by: Irving Lop <irvingloop@gmail.com>

---
+++ app/src/main/java/lopez/irving/kotlinbook/data/db/Tables.kt
@@ -0,0 +1,24 @@
+package lopez.irving.kotlinbook.data.db
+
+/**
+ *
+ * @author irving.lopez
+ * @since 12/03/2018.
+ */
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
